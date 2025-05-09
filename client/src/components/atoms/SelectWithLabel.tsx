a3b38b84893713cec16d04e1063480daed5f5671
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-10 00:35:28 +0530

    updated package.json, import select input and create select with label component

---
+++ client/src/components/atoms/SelectWithLabel.tsx
@@ -0,0 +1,54 @@
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+interface SelectWithLabelProps {
+  label: string;
+  id: string;
+  placeholder: string;
+  disabled?: boolean;
+  items: {
+    id: number;
+    item: string;
+    value: string;
+  }[];
+  required?: boolean;
+  onChange?: (value: string) => void;
+}
+
+const SelectWithLabel = ({
+  label,
+  id,
+  placeholder,
+  items,
+  disabled = false,
+  required = false,
+  onChange,
+}: SelectWithLabelProps) => {
+  return (
+    <div className="space-y-1">
+      <Label htmlFor={id} className="text-sm text-muted-foreground">
+        {label}
+      </Label>
+      <Select disabled={disabled} required={required} onValueChange={onChange}>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {items.map((item) => (
+            <SelectItem key={item.id} value={item.value}>
+              {item.item}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default SelectWithLabel;
