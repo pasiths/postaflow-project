e28ae8116c1efa4c5df7145c999222888672fa6e
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-10 02:14:22 +0530

    correct cookie parser usage and update server listen log to include IP and allow cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,22 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
-import { PORT } from "./secrets";
+import { IP, PORT } from "./secrets";
 import { errorMiddleware } from "./middlewares/errors";
 import rootRouter from "./routes";
 
 const app: Express = express();
 
 app.use(express.json());
-app.use(cookiesParaser());
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use("/api", rootRouter);
 app.get("/test", (req, res) => {
@@ -24,6 +31,6 @@ export const prisma = new PrismaClient();
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, IP,() => {
+  console.log(`Server is running on http://${IP}:${PORT}`);
 });
