fecbc5544b48adc012b720a983cad2ecee9e2faf
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-09 12:38:34 +0530

    feat: update dependencies and improve project structure
    - Added new dependencies: class-variance-authority, clsx, lucide-react, tailwind-merge.
    - Removed postcss.config.js and tailwind.config.js as they are no longer needed.
    - Updated App component to display a simple greeting.
    - Refactored index.css to use @import for Tailwind and tw-animate-css, and added custom properties for theming.
    - Introduced utility function `cn` for class name merging using clsx and tailwind-merge.
    - Enhanced TypeScript configuration with baseUrl and paths for easier imports.
    - Updated Vite configuration to include Tailwind CSS plugin and set up path aliasing.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite"
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+  },
+});
