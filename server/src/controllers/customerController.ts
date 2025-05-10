fe6ed1e67ade32d41b5789aa563b69823a459c24
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-10 17:30:51 +0530

    Enhance customer retrieval with error handling for no results found

--- server/src/controllers/customerController.ts
+++ server/src/controllers/customerController.ts
@@ -4,6 +4,41 @@ import { prisma } from "..";
 import { BadRequestException } from "../exceptions/bad-request";
 import { ErrorCode } from "../exceptions/root";
 
+export const GetCustomers = async (req: Request, res: Response) => {
+  const { firstName, lastName, email } = req.query;
+
+  const customers = await prisma.customer.findMany({
+    where: {
+      AND: [
+        firstName
+          ? { fName: { contains: firstName as string, mode: "insensitive" } }
+          : {},
+        lastName
+          ? { lName: { contains: lastName as string, mode: "insensitive" } }
+          : {},
+        email
+          ? { email: { contains: email as string, mode: "insensitive" } }
+          : {},
+      ],
+    },
+  });
+
+  if (customers.length === 0) {
+    throw new BadRequestException(
+      "No customers found!",
+      ErrorCode.CUSTOMER_NOT_FOUND
+    );
+  }
+
+  console.log(
+    `LOG_BOOK customer= ${firstName} ${lastName} ${email} searched by ${
+      req.user?.username
+    } at ${new Date().toLocaleString()}`
+  );
+
+  res.json({ customers: customers });
+};
+
 export const CreateCustomer = async (req: Request, res: Response) => {
   CustomerSchema.parse(req.body);
 
--- server/src/routes/customerRoute.ts
+++ server/src/routes/customerRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { CreateCustomer } from "../controllers/customerController";
+import { CreateCustomer, GetCustomers } from "../controllers/customerController";
 import { authMiddleware, requirePostal } from "../middlewares/authMiddleware";
 
 
 const customerRoute : Router = Router();
 
 customerRoute.use(errorHandler(authMiddleware))
+customerRoute.get("/",errorHandler(requirePostal), errorHandler(GetCustomers))
 customerRoute.post("/",errorHandler(requirePostal), errorHandler(CreateCustomer))
 
 export default customerRoute;
